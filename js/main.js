4b34ad7054208d35c755e66df4d9fdcc7f93c237
Eduardo <fmendoza094@gmail.com>
2024-10-23 21:11:28 -0500

    suma

---
+++ js/main.js
@@ -0,0 +1,44 @@
+/*Función: Es un bloque de codigo independiente
+que se lo puede usar en cualquier parte de mi programa
+para que sirve: facilitar el código
+reutilizar el código
+sintaxis básica: funtion nombre_funtion(){
+bloque de código
+}*/
+
+/*function saludo(nombre){
+    document.write("Hola "  + nombre)
+}
+//llamamos a la función
+saludo("Eduardo" + "<br>")
+
+function suma(numero1, numero2){
+    //inicializamos las variables a utilizar
+    var numero1=numero1
+    var numero2=numero2
+    //document.write(numero1+numero2)
+    var resultado=numero1+numero2
+   //document.write(resultado)
+  //return resultado
+  return numero1+numero2
+}
+document.write(suma(10, 30))*/
+
+var maximo= function maximo(valor1, valor2){
+    if (valor1>valor2){
+        return valor1
+    }else{
+        return valor2
+    }
+}
+
+maximo (50,25)
+document.write( "<br>"+ "el número mayor es " + maximo(25,2))
+
+var suma= function(numero1, numero2){
+    var numero1=parseInt(document.getElementByID("numero1").value);
+    var numero2=parseInt(document.getElementByID("numero2").value);
+    var resultado= numero1+numero2
+    return resultado
+}
+//document.write(suma(10,2))
